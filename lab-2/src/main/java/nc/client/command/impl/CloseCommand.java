ab9be869f19cb200018ad3f0418e699465526de6
Andrei Matusevich <matusievic@gmail.com>
2018-10-10 23:39:13 +0300

    Implement CLOSE command

--- lab-2/src/main/java/nc/client/command/impl/CloseCommand.java
+++ lab-2/src/main/java/nc/client/command/impl/CloseCommand.java
@@ -8,15 +8,7 @@ import java.net.DatagramSocket;
 public class CloseCommand implements ClientCommand {
     @Override
     public void execute(DatagramSocket client, String command) throws IOException {
-        /*PrintWriter output = new PrintWriter(client.getOutputStream());
-        output.write("CLOSE\n");
-        output.flush();
-
-        Scanner input = new Scanner(client.getInputStream());
-        if (input.hasNext()) {
-            System.out.println(input.nextLine());
-        }
-
-        client.close();*/
+        System.out.println("RESPONSE > CLOSED");
+        System.exit(0);
     }
 }
--- lab-2/src/main/java/nc/server/command/impl/CloseCommand.java
+++ lab-2/src/main/java/nc/server/command/impl/CloseCommand.java
@@ -8,12 +8,5 @@ import java.net.SocketAddress;
 import java.util.List;
 
 public class CloseCommand implements ServerCommand {
-    public void execute(DatagramSocket server, SocketAddress address, List<byte[]> datagrams) throws IOException {
-        /*Writer output = new PrintWriter(server.getOutputStream());
-        System.out.println("CLIENT: CLOSE");
-        output.write("SERVER: Connection is closed\n");
-        output.flush();
-        server.close();
-        */System.out.println("INFO: The connection is closed");
-    }
+    public void execute(DatagramSocket server, SocketAddress address, List<byte[]> datagrams) throws IOException {}
 }
